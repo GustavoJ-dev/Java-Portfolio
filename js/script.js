e98897f398d8e1184582caccdfa8577b1fa38714
Gustavo De Jesus Silva <gustavo.jesus.silva.dev@gmail.com>
2026-05-22 08:57:52 -0300

    feat: implementei a itereção do menu superior

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 // Smooth scrolling for navigation links
+const nav = document.querySelector('nav');
+const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelectorAll('nav a');
 navLinks.forEach(link =>{
     link.addEventListener('click', () => {
@@ -6,4 +8,10 @@ navLinks.forEach(link =>{
         const section = document.querySelector(sectionId);
         section.scrollIntoView({ behavior: 'smooth' });
     })
+    nav.classList.remove('active');
+});
+
+/**menu mobile */
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
 });
